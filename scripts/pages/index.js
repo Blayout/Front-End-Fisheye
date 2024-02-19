0c44bc3455810079c43b0478f7614ded8a3b4f09
Bruh <108009787+Blayout@users.noreply.github.com>
2024-02-19 18:43:19 +0100

    commit test

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,19 +1,18 @@
 async function getPhotographers() {
   try {
-    const response = await fetch('../../data/photographers.json');
+    const response = await fetch("../../data/photographers.json");
     const photographers = await response.json();
     return photographers;
   } catch (error) {
-    console.error('Erreur lors de la récupération des photographes :', error);
+    console.error("Erreur lors de la récupération des photographes :", error);
     throw error;
   }
 }
 
-// Utilisez la fonction pour récupérer les photographes
 getPhotographers()
   .then((photographers) => {
-    console.log('Photographers data:', photographers);
+    console.log("Photographers data:", photographers);
   })
   .catch((error) => {
-    console.log('pas de photographe', error)
+    console.log("pas de photographe", error);
   });
